a4d19c19b1470e4087c445d8eeb559fc2e102656
tfesalmbp <tfesal@naver.com>
2021-08-15 23:14:22 +0900

    ChinesezodiacLoveType JS

--- js/start.js
+++ js/start.js
@@ -2,54 +2,17 @@
 const qna = document.querySelector("#qna");
 const resutl = document.querySelector("#result");
 const endPoint = 12;
-const select = [];
+const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function calResult(){
-  var pointArray = [
-    {name: 'mouse', value: 0, key: 0},
-    {name: 'cow', value: 0, key: 1},
-    {name: 'tiger', value: 0, key: 2},
-    {name: 'rabbit', value: 0, key: 3},
-    {name: 'dragon', value: 0, key: 4},
-    {name: 'snake', value: 0, key: 5},
-    {name: 'horse', value: 0, key: 6},
-    {name: 'sheep', value: 0, key: 7},
-    {name: 'monkey', value: 0, key: 8},
-    {name: 'chick', value: 0, key: 9},
-    {name: 'dog', value: 0, key: 10},
-    {name: 'pig', value: 0, key: 11}
-
-  ]
-
-  for(let i = 0; i < endPoint; i++){
-    var target = qnaList[i].a[select[i]];
-
-    for(let j = 0; j < target.type.length; j++){
-      for(let k = 0; k < pointArray.length; k++){
-        if(target.type[j] === pointArray[k].name){
-          pointArray[k].value =  value + 1;
-        }
-      }
-    }
-  }
-
-
-  var resultArray = pointArray.sort(function(a,b){
-    if(a.value > b.value){
-      return -1;
-    }
-    if(a.value < b.value){
-      return 1;
-    }
-    return 0;
-  }) 
-
-
-  console.log(resultArray)
-  let resultword = resultArray[0].key;
-  return resultword;
-
+  
+console.log(select);
+  
+var result = select.indexOf(Math.max(...select));
+  
+return result;
 }
+
 
 function setResult(){
 
