93b846a1038d999e3d6ff7f167bd39b48dbfa8b7
tfesalmbp <tfesal@naver.com>
2021-08-17 23:39:07 +0900

    ChinesezodiacLoveType JS

--- js/share.js
+++ js/share.js
@@ -1,4 +1,4 @@
-﻿const url = 'https://chinesezodiaclovetype.netlify.app/';
+const url = 'https://chinesezodiaclovetype.netlify.app/';
 
 function setShare(){
   var resultImg = document.querySelector('#resultImg');
--- js/start.js
+++ js/start.js
@@ -1,38 +1,54 @@
-﻿const main = document.querySelector("#main");
+const main = document.querySelector("#main");
+
 const qna = document.querySelector("#qna");
-const resutl = document.querySelector("#result");
+
+const result = document.querySelector("#result");
+
+
 const endPoint = 12;
+
 const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+
 
 function calResult(){
   
 console.log(select);
   
 var result = select.indexOf(Math.max(...select));
-  
-return result;
+  return result;
 }
 
 
-function setResult(){
 
-  let point = calResult(); 
-  const resultName = document.querySelector('.resultname');
-  resultName.innerHTML = infoList[point].name;
-  var resultImg = document.createElement('img');
-  const imgDiv = document.querySelector('#resultImg');
-  var imgURL = 'img/image-' + point + '.png';
+function setResult(){
+  
+let point = calResult();
+  
+const resultName = document.querySelector('.resultname');
+  
+resultName.innerHTML = infoList[point].name;
+
+  
+var resultImg = document.createElement('img');
+  
+const imgDiv = document.querySelector('#resultImg');
+  
+var imgURL = 'img/image-' + point + '.png';
   resultImg.src = imgURL;
-  resultImg.alt = point;
+  
+resultImg.alt = point;
   resultImg.classList.add('img-fluid');
-  imgDiv.appendChild(resultImg);
+  
+imgDiv.appendChild(resultImg);
 
   const resultDesc = document.querySelector('.resultDesc');
   resultDesc.innerHTML = infoList[point].desc;
 }
 
 
-function goResult() {
+
+function goResult(){
   qna.style.WebkitAnimation = "fadeOut 1s";
   qna.style.animation = "fadeOut 1s";
   setTimeout(() => {
@@ -40,16 +56,16 @@ function goResult() {
     result.style.animation = "fadeIn 1s";
     setTimeout(() => {
       qna.style.display = "none";
-      result.style.display = "block";
-    }, 350)})
-  }
+      result.style.display = "block"
+    }, 450)})
+    setResult();
+}
 
-  calResult();
+
 
 function addAnswer(answerText, qIdx, idx){
-	var a = document.querySelector('.answerBox'); 
-	var answer = document.createElement('button');
-	
+  var a = document.querySelector('.answerBox');
+  var answer = document.createElement('button');
   answer.classList.add('answerList');
   answer.classList.add('my-3');
   answer.classList.add('py-3');
@@ -58,41 +74,46 @@ function addAnswer(answerText, qIdx, idx){
 
   a.appendChild(answer);
   answer.innerHTML = answerText;
-  
+
   answer.addEventListener("click", function(){
-    var children = document.querySelectorAll('.answerList');  
+    var children = document.querySelectorAll('.answerList');
     for(let i = 0; i < children.length; i++){
-      children[i].disabled = true;          
-      children[i].style.WebkitAnimation = "fadeOut 0.5s";          
+      children[i].disabled = true;
+      children[i].style.WebkitAnimation = "fadeOut 0.5s";
       children[i].style.animation = "fadeOut 0.5s";
+    }
+    setTimeout(() => {
+      var target = qnaList[qIdx].a[idx].type;
+      for(let i = 0; i < target.length; i++){
+        select[target[i]] += 1;
       }
-      
-  setTimeout(() => {
-    var target = qnaList[qIdx].a[idx].type;
-    for(let i = 0; i < children.length; i++){          
-      children[i].style.display = 'none';
+
+      for(let i = 0; i < children.length; i++){
+        children[i].style.display = 'none';
       }
-        
       goNext(++qIdx);
-      },450)
-    }, false);
+    },450)
+  }, false);
 }
- 
+
+
+
 function goNext(qIdx){
   if(qIdx === endPoint){
     goResult();
     return;
   }
+
   var q = document.querySelector('.qBox');
   q.innerHTML = qnaList[qIdx].q;
   for(let i in qnaList[qIdx].a){
     addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
   }
-
   var status = document.querySelector('.statusBar');
   status.style.width = (100/endPoint) * (qIdx+1) + '%';
-
 }
+
+
 
 function begin(){
   main.style.WebkitAnimation = "fadeOut 1s";
@@ -101,11 +122,10 @@ function begin(){
     qna.style.WebkitAnimation = "fadeIn 1s";
     qna.style.animation = "fadeIn 1s";
     setTimeout(() => {
-      main.style.display = "none"; 
-      qna.style.display = "block";
-    }, 350)
+      main.style.display = "none";
+      qna.style.display = "block"
+    }, 450)
     let qIdx = 0;
     goNext(qIdx);
-  }, 350);
+  }, 450);
 }
-
